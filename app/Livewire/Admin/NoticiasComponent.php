281f1d1f4daec4d9efea2c7d329e6519d6cc2bb1
julio-DevI <julio.a.valenca@gmail.com>
2023-11-13 16:06:16 -0300

    create admin-dashboard and noticias component

---
+++ app/Livewire/Admin/NoticiasComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class NoticiasComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.noticias-component');
+    }
+}
