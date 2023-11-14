c0c363d1fa65bf83ff7d7f3c391b1819fbe2a856
julio-DevI <julio.a.valenca@gmail.com>
2023-11-14 15:04:50 -0300

    create method for add the noticia

--- app/Livewire/Admin/AddNoticiasComponent.php
+++ app/Livewire/Admin/AddNoticiasComponent.php
@@ -2,10 +2,46 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Noticia;
 use Livewire\Component;
 
 class AddNoticiasComponent extends Component
 {
+    public $titulo;
+    public $subtitulo;
+    public $descricao;
+    public $user_id;
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'titulo' => 'required',
+            'subtitulo' => 'required',
+            'descricao' => 'required',
+            'user_id' => 'required'
+        ]);
+    }
+
+    public function adicionaNoticia()
+    {
+        $this->validate([
+            'titulo' => 'required',
+            'subtitulo' => 'required',
+            'descricao' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        $noticia = new Noticia();
+
+        $noticia->user_id = $this->user_id;
+        $noticia->titulo = $this->titulo;
+        $noticia->subtitulo = $this->subtitulo;
+        $noticia->descricao = $this->descricao;
+
+        $noticia->save();
+        session()->flash('message', 'Noticia adicionada com sucesso!');
+    }
+
     public function render()
     {
         return view('livewire.admin.add-noticias-component')->layout('layouts.admin-layout');
