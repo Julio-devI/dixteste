3b1f089aebe78a0da9cb97403af662753f3228fb
julio-DevI <julio.a.valenca@gmail.com>
2023-11-14 23:10:35 -0300

    create editnoticias method

--- app/Livewire/Admin/EditNoticiasComponent.php
+++ app/Livewire/Admin/EditNoticiasComponent.php
@@ -2,10 +2,58 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Noticia;
 use Livewire\Component;
 
 class EditNoticiasComponent extends Component
 {
+    public $noticia_id;
+    public $titulo;
+    public $subtitulo;
+    public $descricao;
+    public $user_id;
+
+    public function mount($noticia_id)
+    {
+        $noticia = Noticia::find($noticia_id);
+
+        $this->noticia_id = $noticia->id;
+        $this->titulo = $noticia->titulo;
+        $this->subtitulo = $noticia->subtitulo;
+        $this->descricao = $noticia->descricao;
+        $this->user_id = $noticia->user_id;
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'titulo' => 'required',
+            'subtitulo' => 'required',
+            'descricao' => 'required',
+            'user_id' => 'required'
+        ]);
+    }
+
+    public function updateNoticia()
+    {
+        $this->validate([
+            'titulo' => 'required',
+            'subtitulo' => 'required',
+            'descricao' => 'required',
+            'user_id' => 'required'
+        ]);
+
+        $noticia = Noticia::find($this->noticia_id);
+
+        $noticia->titulo = $this->titulo;
+        $noticia->subtitulo = $this->subtitulo;
+        $noticia->descricao = $this->descricao;
+        $noticia->user_id = $this->user_id;
+
+        $noticia->save();
+        session()->flash('message', 'Noticia foi adicionada com sucesso');
+    }
+
     public function render()
     {
         return view('livewire.admin.edit-noticias-component');
