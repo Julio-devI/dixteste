c895f85b6a694b5d94160e458dcf8613ac8124b2
julio-DevI <julio.a.valenca@gmail.com>
2023-11-15 08:42:04 -0300

    create frontend for edit usuarios

--- resources/views/livewire/admin/edit-usuarios-component.blade.php
+++ resources/views/livewire/admin/edit-usuarios-component.blade.php
@@ -1,3 +1,174 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="layout-page">
+    <!-- Navbar -->
+    <nav
+        class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+        id="layout-navbar">
+        <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+            <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+            </a>
+        </div>
+
+        <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+            <!-- Search -->
+            <div class="navbar-nav align-items-center">
+                <div class="nav-item d-flex align-items-center">
+                    <i class="bx bx-search fs-4 lh-0"></i>
+                    <input
+                        type="text"
+                        class="form-control border-0 shadow-none ps-1 ps-sm-2"
+                        placeholder="Search..."
+                        aria-label="Search..." />
+                </div>
+            </div>
+            <!-- /Search -->
+
+            <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- Place this tag where you want the button to render. -->
+                <li class="nav-item lh-1 me-3">
+                    <a
+                        class="github-button"
+                        href="https://github.com/themeselection/sneat-html-admin-template-free"
+                        data-icon="octicon-star"
+                        data-size="large"
+                        data-show-count="true"
+                        aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
+                    >Star</a
+                    >
+                </li>
+
+                <!-- User -->
+                <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                        <div class="avatar avatar-online">
+                            <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <div class="d-flex">
+                                    <div class="flex-shrink-0 me-3">
+                                        <div class="avatar avatar-online">
+                                            <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                                        </div>
+                                    </div>
+                                    <div class="flex-grow-1">
+                                        <span class="fw-medium d-block">John Doe</span>
+                                        <small class="text-muted">Admin</small>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="dropdown-divider"></div>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="bx bx-user me-2"></i>
+                                <span class="align-middle">My Profile</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                <i class="bx bx-cog me-2"></i>
+                                <span class="align-middle">Settings</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                        <span class="d-flex align-items-center align-middle">
+                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                          <span class="flex-grow-1 align-middle ms-1">Billing</span>
+                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                        </span>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="dropdown-divider"></div>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="javascript:void(0);">
+                                <i class="bx bx-power-off me-2"></i>
+                                <span class="align-middle">Log Out</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <!--/ User -->
+            </ul>
+        </div>
+    </nav>
+
+    <!-- / Navbar -->
+
+    <!-- Content wrapper -->
+    <div class="content-wrapper">
+        <!-- Content -->
+
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <h4 class="py-3 mb-4"><span class="text-muted fw-light">Adicionar /</span> Usuario</h4>
+
+            <div class="row">
+                @if(Session::has('message'))
+                    <div class="alert alert-success" role="alert">
+                        {{ Session::get('message') }}
+                    </div>
+                @endif
+
+                <div class="col-xl-6">
+                    <!-- HTML5 Inputs -->
+                    <div class="card mb-4">
+                        <h5 class="card-header">Adicionar</h5>
+                        <div class="card-body">
+                            <form wire:submit.prevent='adicionaUsuario' action="">
+                                <div class="mb-3 row">
+                                    <label for="nome" class="col-md-2 col-form-label">Nome</label>
+                                    <div class="col-md-10">
+                                        <input class="form-control" type="text" placeholder="Insira o nome do usuario" name="nome" wire:model="name"/>
+                                        @error('nome')
+                                        <p class="text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="email" class="col-md-2 col-form-label">E-mail</label>
+                                    <div class="col-md-10">
+                                        <input class="form-control" type="text" placeholder="Insira o email do usuario" name="email" wire:model="email"/>
+                                        @error('email')
+                                        <p class="text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="password" class="col-md-2 col-form-label">Password</label>
+                                    <div class="col-md-10">
+                                        <input class="form-control" type="password" placeholder="Insira a senha" name="password" wire:model="password"/>
+                                        @error('password')
+                                        <p class="text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="usertype" class="col-md-2 col-form-label">Tipo de usuario</label>
+                                    <div class="col-md-10">
+                                        <input class="form-control" type="text" placeholder="Insira o id do usuario associado a noticia" name="user type"  wire:model="utype"/>
+                                        @error('user_id')
+                                        <p class="text-danger">{{ $message }}</p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <button type="submit" class="btn btn-primary float-end">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- / Content -->
+        <div class="content-backdrop fade"></div>
+    </div>
+    <!-- Content wrapper -->
 </div>
