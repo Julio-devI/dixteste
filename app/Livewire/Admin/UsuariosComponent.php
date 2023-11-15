30bf5d54f6da341f702ff31cc35594bf8ddcb93d
julio-DevI <julio.a.valenca@gmail.com>
2023-11-15 09:07:04 -0300

    configure Component for all users

--- app/Livewire/Admin/UsuariosComponent.php
+++ app/Livewire/Admin/UsuariosComponent.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UsuariosComponent extends Component
 {
+    public $usuario_id;
+
+    public function deleteUsuario()
+    {
+        $usuario = User::find($this->usuario_id);
+        $usuario->delete();
+        session()->flash('message', 'Usuario deletado com sucesso');
+    }
     public function render()
     {
-        return view('livewire.admin.usuarios-component');
+        return view('livewire.admin.usuarios-component', ['usuarios' => User::all()])->layout('layouts.admin-layout');
     }
 }
