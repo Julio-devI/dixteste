eb1f4126665034f2e285a35a5bdfb7fc606f3783
julio-DevI <julio.a.valenca@gmail.com>
2023-11-15 08:57:55 -0300

    create add usuarios component

--- app/Livewire/Admin/AddUsuariosComponent.php
+++ app/Livewire/Admin/AddUsuariosComponent.php
@@ -2,12 +2,45 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;
 use Livewire\Component;
 
 class AddUsuariosComponent extends Component
 {
+    public $nome;
+    public $email;
+    public $password;
+    public $utype;
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'nome' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+    }
+
+    public function adicionaUsuario()
+    {
+        $this->validate([
+            'nome' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $usuario = new User();
+        $usuario->name = $this->nome;
+        $usuario->email = $this->email;
+        $usuario->password = $this->password;
+        $usuario->utype = $this->utype;
+
+        $usuario->save();
+        session()->flash('message', 'Usuario adicionado com sucesso');
+    }
+
     public function render()
     {
-        return view('livewire.admin.add-usuarios-component');
+        return view('livewire.admin.add-usuarios-component')->layout('layouts.admin-layout');
     }
 }
